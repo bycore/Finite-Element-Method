871a87632c7c7f76c86a651331c411ca8aad52c2
boyce <boyce.core@gmail.com>
2021-03-07 15:15:47 +0800

    add 2D init

--- Gaussian_integral.py
+++ Gaussian_integral.py
@@ -4,8 +4,8 @@ import sympy
 
 def funtest1(x):
     return x
-def funtest2(x):
-    return x
+def funtest2(x,y):
+    return x*x+y
 
 
 def Gau_3(fun,a=-1.0,b=1.0):
@@ -26,4 +26,18 @@ def Gau_sym_3(fun,a=-1.0,b=1.0):
     GauSum=GauSum*(b-a)/2
     return GauSum*sng
 
+def Gau_2D_9(fun):
+    gpt=math.sqrt(3.0/5)
+    A=[25.0/81,25.0/81,25.0/81,25.0/81,40.0/81,40.0/81,40.0/81,40.0/81,64.0/81]
+    x=[-gpt,gpt,gpt,-gpt,0,gpt,0,-gpt,0]
+    y=[-gpt,-gpt,gpt,gpt,-gpt,0,gpt,0,0]
+    GauSum=0.0
+    for i in range(9):
+        GauSum+=fun(x[i],y[i])*A[i]
+    return GauSum
+
 x=sympy.symbols("x")
+
+if __name__== '__main__':
+    init=Gau_2D_9(funtest2)
+    print(init)
